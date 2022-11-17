0a0b451bb9e4e6d86fb1df680b895893d0a6c5e3
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-17 16:15:54 +0100

    array con oggetti

---
+++ script.js
@@ -0,0 +1,32 @@
+let membri = [
+    {
+        nome: "Angela Carol",
+        ruolo: "Chief Editor",
+        immagine: "img/angela-carol-chief-editor.jpg"
+    },
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        immagine: "img/angela-lopez-social-media-manager.jpg"
+    },
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        immagine: "img/barbara-ramos-graphic-designer.jpg"
+    },
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        immagine: "img/scott-estrada-developer.jpg"
+    },
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        immagine: "img/walter-gordon-office-manager.jpg"
+    },
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder CEO",
+        immagine: "img/wayne-barnett-founder-ceo.jpg"
+    }
+]
